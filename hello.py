16e2ae47242ae683a94e173ca1b0c8fa0860d355
smari steinar <smarikul@hotmail.com>
2019-10-09 10:50:28 +0000

    ari
    Signed-off-by: smari steinar <smarikul@hotmail.com>

--- hello.py
+++ hello.py
@@ -1,11 +1,23 @@
 from easygui import *
-
+from playsound import *
+from pydub import AudioSegment
+import pyglet
 
 msg = "song:"
 
-btns = ["paus", "play"]
+btns = ["paus", "play", "quit"]
+
+music = pyglet.resource.media('1.mp3')
 
 
-buttonbox(msg,choices = btns)
 
-exit(0)
+while True:
+    flag1 = buttonbox(msg,choices = btns)
+    if flag1 == "play":
+        music.play()
+    if flag1 == "paus":
+        print("music is now paused")
+    if flag1 == "quit":
+        exit(0)
+
+pyglet.app.run()
---
+++ test.py
@@ -0,0 +1,9 @@
+
+
+
+import pyglet
+
+music = pyglet.media.load("1.mp3")
+music.play()
+
+pyglet.app.run()
